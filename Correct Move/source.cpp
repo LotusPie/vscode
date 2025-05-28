51756e945e04bf7b7a0b78aa09a54e3d8491862f
LotusPie <po0919304578@gmail.com>
2025-05-28 19:34:53 +0800

    Refactor code structure for improved readability and maintainability

--- Correct Move/source.cpp
+++ Correct Move/source.cpp
@@ -3,17 +3,14 @@
 
 using namespace std;
 
-int m[8][8]; // map
-int dir[4][2]={{0,1},{0,-1},{1,0},{-1,0}}; // diration
-
-bool isVerticle(int Qx,int qx,int Qy,int qy){
+bool is_verticle(int Qx,int qx,int Qy,int qy){
     if((Qx==qx&&Qy!=qy)||(Qx!=qx&&Qy==qy)){
         return true;
     }
     else return false;
 }
 
-bool isPathBlock(int Qx,int qx,int Qy,int qy,int Kx,int Ky){
+bool is_path_block(int Qx,int qx,int Qy,int qy,int Kx,int Ky){
     if(Kx == Qx){
         if((Ky<Qy&&Ky>qy)||(Ky<qy&&Ky>Qy)){
             return true;
@@ -23,6 +20,19 @@ bool isPathBlock(int Qx,int qx,int Qy,int qy,int Kx,int Ky){
             return true;
         }
     }
+    return false;
+}
+
+bool is_king_move_pattern(int kr, int kc, int tr, int tc) {
+    int dr = abs(kr - tr);
+    int dc = abs(kc - tc);
+    return (dr == 1 && dc == 0) || (dr == 0 && dc == 1);
+}
+
+bool is_queen_move_pattern(int Qr, int Qc, int qr, int qc) {
+    bool is_horizontal_move = (Qr == qr && Qc != qc);
+    bool is_vertical_move = (Qc == qc && Qr != qr);
+    return is_horizontal_move || is_vertical_move;
 }
 
 int main(){
@@ -32,20 +42,56 @@ int main(){
             cout<<"Illegal state"<<endl;
             continue;
         }
-        for(int i = 0;i<8;i++){ 
-            for(int j = 0;j<8;j++){
-                m[i][j]= 0;
-            }
+        int Qc = b % 8,Qr = b / 8, qc = c % 8,qr = c / 8;
+        int kc = a % 8,kr = a / 8;
+        if(!is_verticle(Qc,qc,Qr,qr)){ // condition two
+            cout<<"Illegal move"<<endl;
+            continue;
+        }else if(qc == kc&&qr == kr){
+            cout<<"Illegal move"<<endl;
+            continue;
+        }else if(is_path_block(Qc,qc,Qr,qr,kc,kr)){
+            cout<<"Illegal move"<<endl;
+            continue;
         }
-        int Q_x = b % 8,Q_y = b / 8,q_x = c % 8,q_y = c / 8;
-        int K_x = a % 8,K_y = a / 8;
-        if(!isVerticle(Q_x,q_x,Q_y,q_y)){ // condition two
-            cout<<"Illegal move"<<endl;
-        }else if(q_x == K_x&&q_y == K_y){
-            cout<<"Illegal move"<<endl;
-        }else if(isPathBlock(Q_x,q_x,Q_y,q_y,K_x,K_y)){
-            cout<<"Illegal move"<<endl;
+        if (is_king_move_pattern(kr, kc, qr, qc)) {
+            cout << "Move not allowed" << endl;
+            continue;
         }
         
-    }
+        bool king_has_an_allowed_move = false;
+        int king_move_directions[4][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}}; // 右, 左, 下, 上
+
+        for (int i = 0; i < 4; ++i) {
+            int k_next_r = kr + king_move_directions[i][0];
+            int k_next_c = kc + king_move_directions[i][1];
+
+            bool is_king_target_on_board = (k_next_r >= 0 && k_next_r < 8 && k_next_c >= 0 && k_next_c < 8);
+            if (!is_king_target_on_board) {
+                continue; 
+            }
+            if (k_next_r == qr && k_next_c == qc) {
+                continue; 
+            }
+            bool queen_can_also_reach_k_next = false; 
+            if (is_verticle(qc, k_next_c, qr, k_next_r)) { 
+                if (!(k_next_r == kr && k_next_c == kc)) {
+                    if (!is_path_block(qc, k_next_c, qr, k_next_r, kc, kr)) { 
+                        queen_can_also_reach_k_next = true; 
+                    }
+                }
+            }
+            if (!queen_can_also_reach_k_next) {
+                king_has_an_allowed_move = true; 
+                break;                           
+            }
+        }
+        if (king_has_an_allowed_move) {
+            cout << "Continue" << endl;
+            continue;
+        } else {
+            cout << "Stop" << endl;
+            continue;
+        }
+    } 
 }
